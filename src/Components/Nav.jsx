77192f90c7bcd9495a7dfc12720f3fa66adad638
General090 <gospelkalu33@gmail.com>
2025-02-11 02:25:26 +0100

    added

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NetflixLogo from "./Assets/netflix-logo.svg";
+
+const Nav = () => {
+  return (
+    <div>
+      <img src={NetflixLogo} alt="Netflix Logo" className="h-10 w-auto" />;
+    </div>
+  )
+}
+
+export default Nav
